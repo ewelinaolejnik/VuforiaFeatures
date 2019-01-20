6a3fe0efc43a3dc01aae3a3dc9458fcbc9698a9e
ewelinaolejnik <ewelina.olejnik@outlook.com>
2019-01-20 21:36:55 +0100

    Image Target recognition and augmented text displayed on the target

---
+++ Assets/Resources/Scripts/Paths.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Resources.Scripts
+{
+    internal static class Paths
+    {
+        public static string VuforiaPath
+        {
+            get
+            {
+                return Path.Combine(Application.persistentDataPath, "Vuforia");
+            }
+        }
+
+        public static string VuforiaDatabasePath
+        {
+            get
+            {
+                var path = Path.Combine(VuforiaPath, "Database");
+                return Directory.GetFiles(path, "*.xml", SearchOption.TopDirectoryOnly).FirstOrDefault(f => f.Contains("ImageTargets"));
+            }
+        }
+    }
+}
